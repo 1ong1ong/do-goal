2d6e873ed25aee85b67d446945f69cdf440a28e3
chenxiaolong <ustb_cxl@163.com>
2020-03-28 17:29:32 +0800

    打卡详情

--- pages/clock-in-detail/index.js
+++ pages/clock-in-detail/index.js
@@ -7,10 +7,64 @@ Page({
    */
   data: {
     height: app.globalData.screenHeight,
-    width: app.globalData.screenWidth / (app.globalData.screenWidth/750)
+    width: app.globalData.screenWidth / (app.globalData.screenWidth / 750),
+    charts: [],
+    current: null
+  },
+
+  onLoad() {
+    let charts = [{
+        date: '3月22日',
+        time: '07:23',
+        percent: 90
+      }, {
+        date: '23',
+        time: '07:23',
+        percent: 40
+      }, {
+        date: '24',
+        time: '07:23',
+        percent: 50
+      }, {
+        date: '25',
+        time: '07:23',
+        percent: 70
+      }, {
+        date: '26',
+        time: '',
+        percent: 0
+      }, {
+        date: '27',
+        time: '07:23',
+        percent: 10
+      }, {
+        date: '28',
+        time: '07:23',
+        percent: 80
+      }
+
+    ];
+    this.setData({
+      charts: charts
+    })
   },
 
   goBackPage() {
     wx.navigateBack();
+  },
+
+  touchPercent(e) {
+    this.setData({
+      current: e.currentTarget.dataset.index
+    })
+  },
+
+  touchPercentEnd() {
+    setTimeout(() => {
+      this.setData({
+        current: null
+      })
+    }, 500)
+
   }
 })
