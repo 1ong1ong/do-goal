748e64f54e74aad78c3e1d76a6aa505bdc4b8b3c
chenxiaolong <ustb_cxl@163.com>
2020-08-22 01:27:37 +0800

    周报分享

--- pages/history-week-report-detail/index.js
+++ pages/history-week-report-detail/index.js
@@ -37,6 +37,27 @@ Page({
         weekReportHisDetail: data
       })
     })
-  }
+  },
+
+  onShareAppMessage(res) {
+    console.log("==============")
+    // let gbid = res.target.dataset.info.order_id;
+    return {
+      title: '分享',
+      path: '/pages/index/index?fromUserId=' + 1,
+      // imageUrl: 'https://......./img/groupshare.png',  //用户分享出去的自定义图片大小为5:4,
+      success: function (res) {
+        // 转发成功
+        wx.showToast({
+          title: "分享成功",
+          icon: 'success',
+          duration: 2000
+        })
+      },
+      fail: function (res) {
+        // 分享失败
+      },
+    }
+  },
 
 })
